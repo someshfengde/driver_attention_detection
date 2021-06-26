a26f73ee0a40b445970d55f77347240f67437f20
someshfengde <someshfengde@gmail.com>
2021-06-26 19:00:18 +0530

    making it as streamlit app

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st 
+from functions import *
+st.write('Driver detection with image')
+
+
+image = st.file_uploader(label='upload image on which you want to detect',type = ['png','jpg'])
+
+
+if image is not None:
+    x = predict_with_image(image)
+    st.write(x)
+else:
+    st.write('upload the image first')
---
+++ functions.py
@@ -0,0 +1,29 @@
+from fastai.vision.all import * 
+from fastai import *  
+from PIL import Image
+
+
+def load_model():
+    def label_func(x): return x.parent.name    
+    model = load_learner('./export.pkl')
+    return model
+
+
+
+
+output_label = {'c0': 'normal driving',
+'c1': 'texting - right',
+'c2': 'talking on the phone - right',
+'c3': 'texting - left',
+'c4': 'talking on the phone - left',
+'c5': 'operating the radio',
+'c6': 'drinking',
+'c7':' reaching behind',
+'c8': 'hair and makeup',
+'c9': 'talking to passenger'}
+
+def predict_with_image(image):
+    model = load_model()
+    prediction  = model.predict(image)
+    label = f'model predicted uploaded image is {output_label[prediction[0]]} with probability {prediction[2].max()}'
+    return label
